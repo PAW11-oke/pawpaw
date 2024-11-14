9dd0c85bf66c88ef961d43466339608cb709d716
adzka <adzka.bagus@gmail.com>
2024-11-14 17:15:07 +0700

    feat: section gallery + article

---
+++ src/app/grooming/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Grooming = () => {
+  return <div></div>;
+};
+
+export default Grooming;
---
+++ src/app/health-tracking/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HealthTracking = () => {
+  return <div></div>;
+};
+
+export default HealthTracking;
