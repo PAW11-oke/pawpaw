c7636f126c5d3ab3fc59156b07d132d7451a0ef3
adzka <adzka.bagus@gmail.com>
2024-11-22 14:42:00 +0700

    feat: auth UI

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -1,19 +1,26 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#FFBCC3] text-[#ffffff] flex flex-col md:flex-row items-center justify-between px-8 rounded-t-[30px]">
-      <div className="text-[16px] text-white mt-4 md:mt-0">&copy; 2024 PawPaw. All rights reserved.</div>
+    <footer className="bg-[#FFBCC3] text-[#ffffff] flex flex-col md:flex-row items-center justify-between px-8 rounded-t-[30px] z-100">
+      <div className="text-[16px] text-white mt-4 md:mt-0">
+        &copy; 2024 PawPaw. All rights reserved.
+      </div>
       <div className="flex items-center">
-        <img src="/icons/FooterLogo.png" alt="Paw Icon" className="h-[140px]" /> {/* Sesuaikan path icon */}
+        <img src="/icons/FooterLogo.png" alt="Paw Icon" className="h-[140px]" />{" "}
+        {/* Sesuaikan path icon */}
       </div>
       <div className="flex space-x-4 text-[16px] text-white mt-4 md:mt-0 gap-[50px]">
-        <a href="/terms" className="hover:underline">Terms of Service</a>
-        <a href="/privacy" className="hover:underline">Privacy Policy</a>
+        <a href="/terms" className="hover:underline">
+          Terms of Service
+        </a>
+        <a href="/privacy" className="hover:underline">
+          Privacy Policy
+        </a>
       </div>
     </footer>
   );
